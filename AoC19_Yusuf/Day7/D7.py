37ae21fcaa475e35527287b7f860b3e58595b6be
yusuf.syakur <yusuf.syakur@usoft.com>
2019-12-20 11:21:20 +0100

    Day 7 Part 2 Sketch 2

--- AoC19_Yusuf/Day7/D7.py
+++ AoC19_Yusuf/Day7/D7.py
@@ -152,27 +152,174 @@ def determineMaxSignal(iArr_input, i_Phase_Min, i_Phase_Max):
 	print("Max signal output is " + str(_iMaxSignal))
 	return _iMaxSignal
 
-def amplify_feedbackloop(iArr_input, iArr_phaseSetting):
-	global first_input_used
-	_iOutputAmp = 0
-	for _iPhase in iArr_phaseSetting:
-		first_input_used = False
-		_iOutputAmp = intCode(iArr_input, 0, _iPhase, _iOutputAmp)
-		if _iOutputAmp == -1:
-			print("Something went wrong!")
-	first_input_used = False
-	return _iOutputAmp
-
 #Get to current directory of puzzle
 #os.chdir(os.getcwd() + r'\Day7')
 
 #Read input
 inp_array = np.loadtxt(fname='D7Input.txt', delimiter=',').astype(int)
 
-print(determineMaxSignal(inp_array, 5, 9))
+print(determineMaxSignal(inp_array, 0, 5))
+
+class Amplifier:
+	iArr_AmpControlSoft = []
+	i_position = 0
+	i_input = 0
+	b_phaseUsed = False
+	b_haltReached = False
+	i_phaseInput = 0
+	i_output = 0
+	s_name = ""
+
+	def __init__(self, s_nameInput, i_phase, i_inputAmp, iArr_inputarray):
+		self.iArr_AmpControlSoft = iArr_inputarray
+		self.i_position = 0
+		self.b_phaseUsed = False
+		self.i_phaseInput = i_phase
+		self.i_input = i_inputAmp
+		self.i_output = 0
+		self.s_name = s_nameInput
+
+	def Start_Intcode(self):
+		_iOpcode = self.iArr_AmpControlSoft[self.i_position]
+		_sParameterMode = ""
+		if _iOpcode > 99:
+			_sParameterMode = str(_iOpcode // 100)
+			_iOpcode = _iOpcode % 100
+			while len(_sParameterMode) < 3:
+				_sParameterMode = f"0{_sParameterMode}"
+		else:
+			_sParameterMode = "000"
+		if(_iOpcode == 99):
+			print("intCode has reached 99. End program!")
+			self.b_haltReached = True
+			return self.i_output
+		elif(_iOpcode == 1):
+			self.opcode_1(_sParameterMode)
+			return self.Start_Intcode()
+		elif(_iOpcode == 2):
+			self.opcode_2(_sParameterMode)
+			return self.Start_Intcode()
+		elif(_iOpcode == 3):
+			self.opcode_3(_sParameterMode)
+			return self.Start_Intcode()
+		elif(_iOpcode == 4):
+			self.opcode_4(_sParameterMode)
+			print("Opcode 4 reached for", self.s_name, "with output", str(self.i_output))
+			return self.i_output
+		elif(_iOpcode == 5):
+			self.opcode_5(_sParameterMode)
+			return self.Start_Intcode()
+		elif(_iOpcode == 6):
+			self.opcode_6(_sParameterMode)
+			return self.Start_Intcode()
+		elif(_iOpcode == 7):
+			self.opcode_7(_sParameterMode)
+			return self.Start_Intcode()
+		elif(_iOpcode == 8):
+			self.opcode_8(_sParameterMode)
+			return self.Start_Intcode()
+		else:
+			print("Something went wrong!")
+			return -1
+
+	def opcode_1(self, s_parameterMode):
+		_iValue1 = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 1]] if s_parameterMode[2] == '0' else self.iArr_AmpControlSoft[self.i_position + 1]
+		_iValue2 = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 2]] if s_parameterMode[1] == '0' else self.iArr_AmpControlSoft[self.i_position + 2]
+		_iValue3 = _iValue1 + _iValue2
+		if s_parameterMode[0] == '0':
+			self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 3]] = _iValue3
+		else:
+			self.iArr_AmpControlSoft[self.i_position + 3] = _iValue3
+		self.i_position += 4
+
+	def opcode_2(self, s_parameterMode):
+		_iValue1 = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 1]] if s_parameterMode[2] == '0' else self.iArr_AmpControlSoft[self.i_position + 1]
+		_iValue2 = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 2]] if s_parameterMode[1] == '0' else self.iArr_AmpControlSoft[self.i_position + 2]
+		_iValue3 = _iValue1 * _iValue2
+		if s_parameterMode[0] == '0':
+			self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 3]] = _iValue3
+		else:
+			self.iArr_AmpControlSoft[self.i_position + 3] = _iValue3
+		self.i_position += 4
+
+	def opcode_3(self, s_parameterMode):
+		_iValueToStore = self.i_input
+		if self.b_phaseUsed == False:
+			self.b_phaseUsed = True
+			print("Opcode 3: Used phase setting once for", self.s_name)
+			_iValueToStore = self.i_phaseInput
+		if s_parameterMode[2] == '0':
+			self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 1]] = _iValueToStore
+		else:
+			self.iArr_AmpControlSoft[self.i_position + 1] = _iValueToStore
+		self.i_position += 2
+
+	def opcode_4(self, s_parameterMode):
+		self.i_output = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 1]] if s_parameterMode[2] == '0' else self.iArr_AmpControlSoft[self.i_position + 1]
+		self.i_position += 2
+
+	def opcode_5(self, s_parameterMode):
+		_iFirstParam = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 1]] if s_parameterMode[2] == '0' else self.iArr_AmpControlSoft[self.i_position + 1]
+		_iSecondParam = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 2]] if s_parameterMode[1] == '0' else self.iArr_AmpControlSoft[self.i_position + 2]
+		if _iFirstParam != 0:
+			self.i_position = _iSecondParam
+		else:
+			self.i_position += 3
+
+	def opcode_6(self, s_parameterMode):
+		_iFirstParam = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 1]] if s_parameterMode[2] == '0' else self.iArr_AmpControlSoft[self.i_position + 1]
+		_iSecondParam = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 2]] if s_parameterMode[1] == '0' else self.iArr_AmpControlSoft[self.i_position + 2]
+		if _iFirstParam == 0:
+			self.i_position =  _iSecondParam
+		else:
+			self.i_position += 3
+
+	def opcode_7(self, s_parameterMode):
+		_iFirstParam = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 1]] if s_parameterMode[2] == '0' else self.iArr_AmpControlSoft[self.i_position + 1]
+		_iSecondParam = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 2]] if s_parameterMode[1] == '0' else self.iArr_AmpControlSoft[self.i_position + 2]
+		_iValueToStore = 1 if _iFirstParam < _iSecondParam else 0
+		if s_parameterMode[0] == '0':
+			self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 3]] = _iValueToStore
+		else:
+			self.iArr_AmpControlSoft[self.i_position + 3] = _iValueToStore
+		self.i_position += 4
+
+	def opcode_8(self, s_parameterMode):
+		_iFirstParam = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 1]] if s_parameterMode[2] == '0' else self.iArr_AmpControlSoft[self.i_position + 1]
+		_iSecondParam = self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 2]] if s_parameterMode[1] == '0' else self.iArr_AmpControlSoft[self.i_position + 2]
+		_iValueToStore = 1 if _iFirstParam == _iSecondParam else 0
+		if s_parameterMode[0] == '0':
+			self.iArr_AmpControlSoft[self.iArr_AmpControlSoft[self.i_position + 3]] = _iValueToStore
+		else:
+			self.iArr_AmpControlSoft[self.i_position + 3] = _iValueToStore
+		self.i_position += 4
+
+
+def amplify_feedbackloop(iArr_input, iArr_phaseSetting):
+	AmpA = Amplifier("AmpA", iArr_phaseSetting[0], 0, iArr_input)
+	AmpB = Amplifier("AmpB", iArr_phaseSetting[1], AmpA.Start_Intcode(), iArr_input)
+	AmpC = Amplifier("AmpC", iArr_phaseSetting[2], AmpB.Start_Intcode(), iArr_input)
+	AmpD = Amplifier("AmpD", iArr_phaseSetting[3], AmpC.Start_Intcode(), iArr_input)
+	AmpE = Amplifier("AmpE", iArr_phaseSetting[4], AmpD.Start_Intcode(), iArr_input)
+	AmpA.i_input = AmpE.Start_Intcode()
+	while AmpE.b_haltReached == False:
+		AmpB.i_input = AmpA.Start_Intcode()
+		AmpC.i_input = AmpB.Start_Intcode()
+		AmpD.i_input = AmpC.Start_Intcode()
+		AmpE.i_input = AmpD.Start_Intcode()
+		AmpA.i_input = AmpE.Start_Intcode()
+	print(AmpE.iArr_AmpControlSoft)
+	return AmpE.i_output
+#Get to current directory of puzzle
+#os.chdir(os.getcwd() + r'\Day7')
+
+#Read input
+inp_array = np.loadtxt(fname='D7TestInput.txt', delimiter=',').astype(int)
+
+#print(determineMaxSignal(inp_array, 0, 5))
 
 #inp_array = np.loadtxt(fname='D7TestInput.txt', delimiter=',').astype(int)
 
-#print(amplify_feedbackloop(inp_array, [9,7,8,5,6]))
+print(amplify_feedbackloop(inp_array, [9,8,7,6,5]))
 
 #print(determineMaxSignal_feedbackloop(inp_array, 5, 5))
