f8990bc2e7a0c48b44cc5d33835f0b7a75aa2b3d
yusuf.syakur <yusuf.syakur@usoft.com>
2019-12-23 15:09:40 +0100

    Day 8 Part 2

--- AoC19_Yusuf/Day8/D8.py
+++ AoC19_Yusuf/Day8/D8.py
@@ -11,7 +11,7 @@ filetext.close()
 
 i_acreage = 25 * 6
 
-sArr_pictures = [s_input[i:i+n] for i in range(0, len(s_input), n)]
+sArr_pictures = [s_input[i:i+i_acreage] for i in range(0, len(s_input), i_acreage)]
 
 i_FewestDigits = i_acreage
 i_FewestPicture = sArr_pictures[0]
@@ -20,3 +20,31 @@ for sArr_picture in sArr_pictures:
 	if _iTempFewestDigits < i_FewestDigits:
 		i_FewestDigits = _iTempFewestDigits
 		i_FewestPicture = sArr_picture
+
+i_num1_digits = i_FewestPicture.count('1')
+i_num2_digits = i_FewestPicture.count('2')
+
+print(i_num1_digits * i_num2_digits)
+
+def decode_picture(s_input, iTotalWidth, iTotalHeight):
+	sArr_pictures = [s_input[i:i+(iTotalWidth * iTotalHeight)] for i in range(0, len(s_input), iTotalWidth * iTotalHeight)]
+	_iArrTemp = ""
+	for iPixel in range(iTotalWidth * iTotalHeight):
+			for sArr_picture in sArr_pictures:
+				if sArr_picture[iPixel] != '2':
+					_iArrTemp += sArr_picture[iPixel]
+					break
+	_iArrSplit = []
+	for index in range(0, len(_iArrTemp), iTotalWidth):
+		_iArrSplit.append(_iArrTemp[index : index + iTotalWidth])
+	return _iArrSplit
+
+#Get to current directory of puzzle
+#os.chdir(os.getcwd() + r'\Day8')
+
+#Read input
+filetext = open("D8Input.txt", "r")
+s_input = filetext.read()
+filetext.close()
+
+print("\n".join(decode_picture(s_input, 25, 6)))
