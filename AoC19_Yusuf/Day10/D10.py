0e4c787ed053e36e35d4e19bd8ed4bd8289e0eeb
yusuf.syakur <yusuf.syakur@usoft.com>
2019-12-24 17:11:13 +0100

    Day 10 Sketch

---
+++ AoC19_Yusuf/Day10/D10.py
@@ -0,0 +1,12 @@
+import os, math, sys
+import numpy as np
+
+#Get to current directory of puzzle
+#os.chdir(os.getcwd() + r'\Day10')
+
+#Read input
+filetext = open("D10Input.txt", "r")
+s_input = filetext.read()
+filetext.close()
+
+print(s_input)
